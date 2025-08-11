f7e418a7b7d7e6a0bc6ff795843c1201deb0f5a9
らぐち <corekaraweb@gmail.com>
2025-08-11 14:52:41 +0900

    フッターまで完成（動きのある部分以外）

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".drawer-icon").addEventListener("click", () => {
+  document.querySelector(".drawer-icon").classList.toggle("is-checked");
+  document.querySelector(".drawer-content").classList.toggle("is-checked");
+});
